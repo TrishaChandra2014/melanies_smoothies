8638a7758a9d1e3dee0f9c70713afe63413e57d1
TrishaChandra2014 <biswastrisha1@gmail.com>
2025-08-06 09:03:10 -0500

    Update streamlit_app.py
    add Python package library - requests to build and sent REST API calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,10 @@ if ingredients_list:
 
     #st.write(my_insert_stmt)
     #st.stop()
+# new session to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 time_to_insert = st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
