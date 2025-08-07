b7a4fbe16811967d219b704a1b21be11f96c0435
TrishaChandra2014 <biswastrisha1@gmail.com>
2025-08-07 00:03:57 -0500

    Update streamlit_app.py
    test the fruityvice link in app

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,10 +63,10 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + ' Nuitrition Information')
-        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        #st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_of_order)
             values ('""" + ingredients_string + """','""" + name_of_order + """')"""
